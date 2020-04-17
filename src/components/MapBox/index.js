89a0ad0e2a396ad86e8fb8900e24c9e4dcc80787
Madeuss <mateus1two3@gmail.com>
2020-04-17 18:13:58 -0300

    Add more markers colors on map

--- src/components/MapBox/index.js
+++ src/components/MapBox/index.js
@@ -9,21 +9,27 @@ export default function MapBox() {
 	mapboxgl.accessToken = 'pk.eyJ1IjoibWF0ZXVzMXR3bzMiLCJhIjoiY2s4dGhiNWo1MDBhazNmbjI5ejV4Y2FnMiJ9.YpD2TO43vR0764qPQf72qg';
 	
 	var map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/dark-v10',
-	zoom: 1.1,
-	center: [10, 25],
+		container: 'map',
+		style: 'mapbox://styles/mapbox/dark-v10',
+		zoom: 1.1,
+		center: [10, 25],
 	});
 
-const getColorFromConfirmedCount = count => {
-	if (count >= 3000){
-		return 'rgb(228, 71, 71)'
+	const getColorFromConfirmedCount = count => {
+		if (count >= 100000){
+			return 'rgb(228, 71, 71)'
+		}
+		if (count >= 30000 ){
+			return 'rgb(000, 010, 200)'
+		}
+		if (count >= 10000 ){
+			return 'rgb(000, 100, 255)'
+		}
+		if (count >= 1000 ){
+			return 'rgb(000, 180, 255)'
+		}
+		return 'grey'
 	}
-	if (count >= 400 ){
-		return 'rgb(000, 080, 255)'
-	}
-	return 'grey'
-}
 
 fetch("https://coronavirus-tracker-api.herokuapp.com/v2/locations")
 	.then(response => response.json())
@@ -34,9 +40,10 @@ fetch("https://coronavirus-tracker-api.herokuapp.com/v2/locations")
 		locations.forEach(location => {
 			const { coordinates, latest } = location
 
-			new mapboxgl.Marker({
+			 new mapboxgl.Marker({
 				color: getColorFromConfirmedCount(latest.confirmed)
 			})
+			.togglePopup(true)
 			.setLngLat([ coordinates.longitude, coordinates.latitude ])
 			.addTo(map);
 		})
