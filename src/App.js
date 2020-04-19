f6b7c14df35a50810143ac3ad32b6671660f643d
Madeuss <mateus1two3@gmail.com>
2020-04-18 22:49:15 -0300

    Deleting API without info about recovered

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import React from 'react';
 import Routes from './routes'
 
 import DataProvider from "./context/ApiData";
-import CountryProvider from "./context/CountryData";
 
 function App() {
   return (
     <DataProvider>
-      <CountryProvider>
         <Routes />
-      </CountryProvider>
     </DataProvider>
   );
 }
--- src/services/api_country.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api_country = axios.create({
-    baseURL: 'https://coronavirus-tracker-api.herokuapp.com/v2/locations'
-})
-
-export default api_country
